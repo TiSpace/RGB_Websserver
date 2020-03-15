054c755da28fc9294884803c871eecb912ebd589
TiSpace <trnospam02@arcor.de>
2020-03-15 22:37:04 +0100

    Add files via upload

---
+++ RGB_Webserver/setting.h
@@ -0,0 +1,10 @@
+//SSID and Password of your WiFi router
+const char* ssid = "****";
+const char* password = "****";
+ 
+
+ 
+//LED Connections
+const int BlueLED = 16; // D0
+const int RedLED = 5;    // D1 
+const int GreenLED = 4; // D2
